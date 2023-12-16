193a61ee02fada12ef192910c8ca2b6701b26173
dtadres <david.tadres@lifesci.ucsb.edu>
2023-12-15 21:01:30 -0800

    test fly builder on sherlock

--- workflow/brainsss/utils.py
+++ workflow/brainsss/utils.py
@@ -109,7 +109,7 @@ class Printlog():
     '''
     for printing all processes into same log file on sherlock
     '''
-    def __init__(self, logfile):
+    def __init__(self, logfile, fly_id):
         self.logfile = logfile
     def print_to_log(self, message):
         with open(self.logfile, 'a+') as f:
