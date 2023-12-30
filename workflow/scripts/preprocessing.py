2d3701fb0d670a0d1a166db143dc55645cacd556
dtadres <david.tadres@lifesci.ucsb.edu>
2023-12-29 17:22:05 -0800

    test vectorized/preallocated moco algo

--- workflow/scripts/preprocessing.py
+++ workflow/scripts/preprocessing.py
@@ -1666,7 +1666,7 @@ def motion_correction(
                 print('temp ' +repr(temp))
                 print('temp.shape' + repr(temp.shape))
 
-                temp = np.asarray(temp, dtype=np.float32)
+                temp = temp.numpy()
                 print("temp.shape " + repr(temp.shape))
                 print('moco_functional_one' + repr(moco_functional_one.shape))
                 print('temp.dtyp' + repr(temp.dtype))
