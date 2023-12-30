c435f505e52f320f8b4a9020efaa0e297becd5dc
dtadres <david.tadres@lifesci.ucsb.edu>
2023-12-30 12:01:02 -0800

    make h5 file original loop version

--- workflow/scripts/preprocessing.py
+++ workflow/scripts/preprocessing.py
@@ -1281,51 +1281,50 @@ def zscore(fly_directory, dataset_path, zscore_path):
                             f['data'][:, :, :, chunkstart:chunkend] = np.nan_to_num(
                                 zscored)  ### Added nan to num because if a pixel is a constant value (over saturated) will divide by 0
                             # printlog(F"vol: {chunkstart} to {chunkend} time: {time()-t0}")
-            # run both looped and vectorized
-            print('Done looped version')
+            else:
 
-            # I think we don't have to worry about memory too much - since we only work
-            # with one h5 file at a time and 30 minutes at float32 is ~20Gb
-            # Expect a 4D array, xyz and the fourth dimension is time!
-            meanbrain = np.nanmean(data, axis=3)
-            np.save('/oak/stanford/groups/trc/data/David/Bruker/preprocessed/fly_002/vect_meanbrain.npy',
-                    meanbrain)
-            # Might get out of memory error, test!
-            final_std = np.std(data, axis=3)
-            np.save('/oak/stanford/groups/trc/data/David/Bruker/preprocessed/fly_002/vect_final_std.npy',
-                    final_std)
+                # I think we don't have to worry about memory too much - since we only work
+                # with one h5 file at a time and 30 minutes at float32 is ~20Gb
+                # Expect a 4D array, xyz and the fourth dimension is time!
+                meanbrain = np.nanmean(data, axis=3)
+                np.save('/oak/stanford/groups/trc/data/David/Bruker/preprocessed/fly_002/vect_meanbrain.npy',
+                        meanbrain)
+                # Might get out of memory error, test!
+                final_std = np.std(data, axis=3)
+                np.save('/oak/stanford/groups/trc/data/David/Bruker/preprocessed/fly_002/vect_final_std.npy',
+                        final_std)
 
-            ### Calculate zscore and save ###
+                ### Calculate zscore and save ###
 
-            # Calculate z-score
-            # z_scored = (data - meanbrain[:,:,:,np.newaxis])/final_std[:,:,:,np.newaxis]
-            # The above works, is easy to read but makes a copy in memory. Since brain data is
-            # huge (easily 20Gb) we'll avoid making a copy by doing in place operations to save
-            # memory! See docstring for more information
+                # Calculate z-score
+                # z_scored = (data - meanbrain[:,:,:,np.newaxis])/final_std[:,:,:,np.newaxis]
+                # The above works, is easy to read but makes a copy in memory. Since brain data is
+                # huge (easily 20Gb) we'll avoid making a copy by doing in place operations to save
+                # memory! See docstring for more information
 
-            # data will be data-meanbrain after this operation
-            data -= meanbrain[:, :, :, np.newaxis]
-            # Then it will be divided by std which leads to zscore
-            data /= final_std[:, :, :, np.newaxis]
-            # From the docs:
-            # Chunking has performance implications. It’s recommended to keep the total size
-            # of your chunks between 10 KiB and 1 MiB, larger for larger datasets. Also
-            # keep in mind that when any element in a chunk is accessed, the entire chunk
-            # is read from disk
-            with h5py.File(current_zscore_path, "w") as file:
-                dset = file.create_dataset(
-                    "data", data=data
-                )  # , dims, dtype='float32', chunks=False)
+                # data will be data-meanbrain after this operation
+                data -= meanbrain[:, :, :, np.newaxis]
+                # Then it will be divided by std which leads to zscore
+                data /= final_std[:, :, :, np.newaxis]
+                # From the docs:
+                # Chunking has performance implications. It’s recommended to keep the total size
+                # of your chunks between 10 KiB and 1 MiB, larger for larger datasets. Also
+                # keep in mind that when any element in a chunk is accessed, the entire chunk
+                # is read from disk
+                with h5py.File(current_zscore_path, "w") as file:
+                    dset = file.create_dataset(
+                        "data", data=data
+                    )  # , dims, dtype='float32', chunks=False)
 
-            if len(dataset_path) > 1:
-                del data
-                printlog(
-                    "Sleeping for 10 seconds before loading the next functional channel"
-                )
-                time.sleep(
-                    10
-                )  # allow garbage collector to start cleaning up memory before potentially loading
-                # the other functional channel!
+                if len(dataset_path) > 1:
+                    del data
+                    printlog(
+                        "Sleeping for 10 seconds before loading the next functional channel"
+                    )
+                    time.sleep(
+                        10
+                    )  # allow garbage collector to start cleaning up memory before potentially loading
+                    # the other functional channel!
 
     printlog("zscore done")
 
