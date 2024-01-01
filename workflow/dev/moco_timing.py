683d4d4afed371b35e2c522b6fcc7b9f0d4dac53
dtadres <david.tadres@lifesci.ucsb.edu>
2024-01-01 14:15:34 -0800

    test moco timing

--- workflow/dev/moco_timing.py
+++ workflow/dev/moco_timing.py
@@ -165,8 +165,15 @@ def combine_files():
             index_start = int(current_file.name.split('chunks_')[-1].split('-')[0])
             index_end = int(current_file.name.split('.npy')[0].split('-')[-1])
             stitched_anatomy_brain[:,:,:,index_start:index_end] = np.load(current_file)[:,:,:,0:int(index_end-index_start)]
+    # Saving
     savepath = pathlib.Path(imaging_path.parent() + '/moco')
     savepath.mkdir(exist_ok=True, parents=True)
+    aff = np.eye(4)
+    stitched_anatomy_brain_nifty = nib.Nifti1Image(
+        stitched_anatomy_brain, aff
+    )
+    stitched_anatomy_brain.to_filename(pathlib.Path(savepath, 'stitched_ch1.nii'))
+
 
 
 
