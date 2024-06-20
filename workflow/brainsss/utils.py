6b51e020eabd6bf63f46a97cb965e951e9c3d661
David Tadres <dtadres@stanford.edu>
2024-06-20 16:57:53 -0700

    debug flybuilder local

--- workflow/brainsss/utils.py
+++ workflow/brainsss/utils.py
@@ -29,28 +29,6 @@ except ImportError:
     FCNTL_EXISTS = False
     import threading
 
-"""try:
-    # Posix based file locking (Linux, Ubuntu, MacOS, etc.)
-    #   Only allows locking on writable files, might cause
-    #   strange results for reading.
-    import fcntl, os
-    def lock_file(f):
-        if f.writable(): fcntl.lockf(f, fcntl.LOCK_EX)
-    def unlock_file(f):
-        if f.writable(): fcntl.lockf(f, fcntl.LOCK_UN)
-except ModuleNotFoundError:
-    # Windows file locking
-    import msvcrt, os
-    def file_size(f):
-        return os.path.getsize( os.path.realpath(f.name) )
-    def lock_file(f):
-        msvcrt.locking(f.fileno(), msvcrt.LK_RLCK, file_size(f))
-    def unlock_file(f):
-        msvcrt.locking(f.fileno(), msvcrt.LK_UNLCK, file_size(f))"""
-
-
-
-
 def convert_list_of_string_to_posix_path(list_of_strings):
     """
     Unfortunately when input is passed by snakemake, the PosixPaths are converted to strings.
@@ -229,13 +207,14 @@ class Printlog:
 
     def print_to_log(self, message):
         if FCNTL_EXISTS:
+            # Linux
             with open(self.logfile, "a+") as f:
                 fcntl.flock(f, fcntl.LOCK_EX)
                 f.write(message)
                 f.write("\n")
                 fcntl.flock(f, fcntl.LOCK_UN)
         else:
-            # do windows stuff
+            # Windows stuff
             lock = threading.Lock()
             def write_to_file():
                 with lock:
@@ -247,33 +226,6 @@ class Printlog:
             print(message)
             print('\n')
 
-"""# Class for ensuring that all file operations are atomic, treat
-# initialization like a standard call to 'open' that happens to be atomic.
-# This file opener *must* be used in a "with" block.
-class AtomicOpen:
-    # Open the file with arguments provided by user. Then acquire
-    # a lock on that file object (WARNING: Advisory locking).
-    def __init__(self, path, *args, **kwargs):
-        # Open the file and acquire a lock on the file before operating
-        self.file = open(path,*args, **kwargs)
-        # Lock the opened file
-        lock_file(self.file)
-
-    # Return the opened file object (knowing a lock has been obtained).
-    def __enter__(self, *args, **kwargs): return self.file
-
-    # Unlock the file and close the file object.
-    def __exit__(self, exc_type=None, exc_value=None, traceback=None):
-        # Flush to make sure all buffered contents are written to file.
-        self.file.flush()
-        os.fsync(self.file.fileno())
-        # Release the lock on the file.
-        unlock_file(self.file)
-        self.file.close()
-        # Handle exceptions that may have come up during execution, by
-        # default any exceptions are raised to the user.
-        if (exc_type != None): return False
-        else:                  return True"""
 def sbatch(
     jobname,
     script,
